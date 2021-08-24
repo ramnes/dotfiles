bfb6f405cc94b8cc7209ad6cd43f857192c75297
ramnes <contact@ramnes.eu>
2021-08-24 15:26:01 +0200

    Don't depend on egrep to improve portability

--- bash/config.bash
+++ bash/config.bash
@@ -104,7 +104,8 @@ shopt -s globstar
 shopt -s histappend
 
 alias activate='. .venv/bin/activate 2>/dev/null || . .env/bin/activate 2>/dev/null'
-alias clean='rm -vf `find | egrep "(*~|*\.pyc|*\.pyo|\#*\#|*\.class|*_flymake\.py)"` 2>/dev/null'
+alias clean='rm -vf $(find . -name "*~" -or -name "*.pyc" -or -name "*.pyo" -or -name "#*#" \
+             -or -name "*.class" -or -name "*_flymake.py" 2> /dev/null)'
 alias dog='pygmentize -g'
 alias emacs-clean='rm -vf `find ~/.emacs.d/ | grep \.elc`'
 alias emacs-re="emacs-clean && emacs-compile"
