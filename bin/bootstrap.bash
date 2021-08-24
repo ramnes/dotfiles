49e89add92e530edd79220f49e3f5eb89462e234
ramnes <contact@ramnes.eu>
2021-08-25 00:23:20 +0200

    Make sure we're using bash from nix

--- bin/bootstrap.bash
+++ bin/bootstrap.bash
@@ -5,6 +5,13 @@ chmod 700 ~/.ssh
 cp -i ssh ~/.ssh/config
 bash bin/install.bash
 
+if [ $? -eq 0 ]
+then
+    bash_path=$(which bash)
+    grep -q ${bash_path} /etc/shells || sudo sh -c "echo ${bash_path} >> /etc/shells"
+    finger $(whoami) | grep -q ${bash_path} || chsh -s ${bash_path}
+fi
+
 shopt -s expand_aliases
 source ~/.bashrc
 pip install --user -I ipython
