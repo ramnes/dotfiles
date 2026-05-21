0cdb0be0b2639389fad11d65b908c8415aa37b21
Guillaume Gelin <contact@ramnes.eu>
2026-05-21 14:14:01 +0200

    Add jwt-decode

--- bash/config.bash
+++ bash/config.bash
@@ -210,6 +210,10 @@ q() {
     claude -p "$*"
 }
 
+jwt-decode() {
+    jq -R 'split(".") |.[0:2] | map(gsub("-"; "+") | gsub("_"; "/") | gsub("%3D"; "=") | @base64d) | map(fromjson)' <<< $1
+}
+
 csv_pp() {
     if [[ "$2" ]]
     then
