7b1ccf1e55f8d4f5ac414ebc85fdca6728aa5a37
Guillaume Gelin <contact@ramnes.eu>
2026-07-22 11:46:25 +0200

    YOLO

--- bash/config.bash
+++ bash/config.bash
@@ -161,6 +161,7 @@ alias gr='cd $(git root)'
 alias clean='rm -vf $(find . -name "*~" -or -name "*.pyc" -or -name "*.pyo" -or -name "#*#" \
              -or -name "*.class" -or -name "*_flymake.py" 2> /dev/null)'
 alias claude='claude --dangerously-skip-permissions'
+alias codex='codex -a never'
 alias compose="docker compose --ansi always"
 alias dog='pygmentize -g'
 alias emacs-clean='rm -vf `find ~/.emacs.d/ | grep \.elc`'
